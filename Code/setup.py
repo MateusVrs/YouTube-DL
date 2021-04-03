01610c2e3ba875ee782985fa5b2844f54c4bb450
MateusVrs <matucavieira@gmail.com>
2021-04-03 12:43:33 -0300

    Update - 1.0.3

--- Code/setup.py
+++ Code/setup.py
@@ -1,7 +1,7 @@
 from cx_Freeze import setup, Executable
 setup(
     name="YouTube - Download",
-    version="1.0.2",
+    version="1.0.3",
     options={"build_exe": {
         'packages': ["tkinter", "time", "pytube"],
         'include_files': ['Images/youtube.png', 'Images/youtube.ico'],
